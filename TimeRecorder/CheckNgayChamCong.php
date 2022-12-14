7f1484a8c22a210885b3ad9ad8f1e828ccfd17cb
unknown <phamvuminh1103@gmail.com>
2022-12-14 15:32:41 +0700

    Push lan 7

--- TimeRecorder/CheckNgayChamCong.php
+++ TimeRecorder/CheckNgayChamCong.php
@@ -8,13 +8,16 @@
         $GioDen = isset($_POST['GioDen']) ? $_POST['GioDen']: '';
         $Ngay = isset($_POST['Ngay']) ? $_POST['Ngay']: '';
 
-        if(isset($_POST['MaNV']) && isset($_POST['GioDen']) && isset($_POST['Ngay'])  ){
+        if(isset($_POST['MaNV']) && isset($_POST['Ngay'])  ){
 
-            $query = "SELECT GioDen WHERE MaNV = '$MaNV' AND Ngay = '$Ngay' ";
-            if($conn->query($query) == TRUE){
-                 $response['message'] = "done";
-            }else{
-                $response['message'] = "error";
+            $query = "SELECT GioDen FROM bangchamcongngay WHERE MaNV = '$MaNV' AND Ngay = '$Ngay' ";
+            $data = mysqli_query($conn, $query);
+
+            while ($row = mysqli_fetch_assoc($data)){
+             array_push($response, new TimeRecorder(
+
+                 $row['GioDen']
+             ));
             }
         
         }else{
@@ -26,6 +29,17 @@
         $response['error'] = true;
         $response['message'] = "Invalid Request"; 
      }
+    
 
+  class TimeRecorder{
+         public $GioDen;
+     
+      function __construct( $GioDen)
+      {
+          $this->GioDen = $GioDen;
+    
+
+      }
+  }
      echo json_encode($response);
 ?>
